d081f09c8ebc47df148f1f94c36e4f663c7b000c
Arthur <arthgeorgeson@gmail.com>
2025-10-25 16:17:57 +0100

    express fix

--- backend/server.js
+++ backend/server.js
@@ -89,7 +89,7 @@ const path = require("path");
 // Serve React build files
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
